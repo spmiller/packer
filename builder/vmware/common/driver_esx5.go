753d9f1ac34201fbdeadbe37a77c6577dbb78350
Stuart Miller <stuart.miller@alliedtelesis.co.nz>
2017-03-03 10:46:23 +1300

    Add quotes around all filenames to prevent issues with spaces

--- builder/vmware/common/driver_esx5.go
+++ builder/vmware/common/driver_esx5.go
@@ -21,6 +21,7 @@ import (
 	"github.com/mitchellh/packer/helper/communicator"
 	"github.com/mitchellh/packer/packer"
 	gossh "golang.org/x/crypto/ssh"
+	"strconv"
 )
 
 // ESX5 driver talks to an ESXi5 hypervisor remotely over SSH to build
@@ -46,7 +47,7 @@ func (d *ESX5Driver) Clone(dst, src string) error {
 
 	linesToArray := func(lines string) []string { return strings.Split(strings.Trim(lines, "\n"), "\n") }
 
-	d.SetOutputDir(path.Dir(dst))
+	d.SetOutputDir(path.Dir(filepath.ToSlash(dst)))
 	srcVmx := d.datastorePath(src)
 	dstVmx := d.datastorePath(dst)
 	srcDir := path.Dir(srcVmx)
@@ -60,23 +61,27 @@ func (d *ESX5Driver) Clone(dst, src string) error {
 		return fmt.Errorf("Failed to create the destination directory %s: %s", d.outputDir, err)
 	}
 
-	err = d.sh("cp", srcVmx, dstVmx)
+	err = d.sh("cp", strconv.Quote(srcVmx), strconv.Quote(dstVmx))
 	if err != nil {
 		return fmt.Errorf("Failed to copy the vmx file %s: %s", srcVmx, err)
 	}
 
-	filesToClone, err := d.run(nil, "find", srcDir, "! -name '*.vmdk' ! -name '*.vmx' -type f ! -size 0")
+	filesToClone, err := d.run(nil, "find", strconv.Quote(srcDir), "! -name '*.vmdk' ! -name '*.vmx' -type f ! -size 0")
 	if err != nil {
 		return fmt.Errorf("Failing to get the file list to copy: %s", err)
 	}
 	for _, f := range linesToArray(filesToClone) {
-		err := d.sh("cp", f, dstDir)
+		// TODO: linesToArray should really return [] if the string is empty. Instead it returns [""]
+		if (f == "") {
+			continue;
+		}
+		err := d.sh("cp", strconv.Quote(f), strconv.Quote(dstDir))
 		if err != nil {
 			return fmt.Errorf("Failing to copy %s to %s: %s", f, dstDir, err)
 		}
 	}
 
-	disksToClone, err := d.run(nil, "sed -ne 's/.*file[Nn]ame = \"\\(.*vmdk\\)\"/\\1/p'", srcVmx)
+	disksToClone, err := d.run(nil, "sed -ne 's/.*file[Nn]ame = \"\\(.*vmdk\\)\"/\\1/p'", strconv.Quote(srcVmx))
 	if err != nil {
 		return fmt.Errorf("Failing to get the vmdk list to clone %s", err)
 	}
@@ -86,7 +91,7 @@ func (d *ESX5Driver) Clone(dst, src string) error {
 			srcDisk = disk
 		}
 		destDisk := path.Join(dstDir, path.Base(disk))
-		err = d.sh("vmkfstools", "-d thin", "-i", srcDisk, destDisk)
+		err = d.sh("vmkfstools", "-d thin", "-i", strconv.Quote(srcDisk), strconv.Quote(destDisk))
 		if err != nil {
 			return fmt.Errorf("Failing to clone disk %s: %s", srcDisk, err)
 		}
@@ -145,7 +150,7 @@ func (d *ESX5Driver) Register(vmxPathLocal string) error {
 	if err := d.upload(vmxPath, vmxPathLocal); err != nil {
 		return err
 	}
-	r, err := d.run(nil, "vim-cmd", "solo/registervm", vmxPath)
+	r, err := d.run(nil, "vim-cmd", "solo/registervm", strconv.Quote(vmxPath))
 	if err != nil {
 		return err
 	}
@@ -298,14 +303,13 @@ func (ESX5Driver) UpdateVMX(_, password string, port uint, data map[string]strin
 }
 
 func (d *ESX5Driver) CommHost(state multistep.StateBag) (string, error) {
-	config := state.Get("config").(*Config)
-	sshc := config.SSHConfig.Comm
+	sshc := state.Get("sshConfig").(*SSHConfig).Comm
 	port := sshc.SSHPort
 	if sshc.Type == "winrm" {
 		port = sshc.WinRMPort
 	}
 
-	if address := config.CommConfig.Host(); address != "" {
+	if address := d.CommConfig.Host(); address != "" {
 		return address, nil
 	}
 
@@ -314,7 +318,10 @@ func (d *ESX5Driver) CommHost(state multistep.StateBag) (string, error) {
 		return "", err
 	}
 
-	record, err := r.find("Name", config.VMName)
+	spacesToUnderscores := func(string string) string {
+		return strings.Replace(string, " ", "_", -1)
+	}
+	record, err := r.find("Name", spacesToUnderscores(d.VMName))
 	if err != nil {
 		return "", err
 	}
@@ -365,12 +372,12 @@ func (d *ESX5Driver) CommHost(state multistep.StateBag) (string, error) {
 //-------------------------------------------------------------------
 
 func (d *ESX5Driver) DirExists() (bool, error) {
-	err := d.sh("test", "-e", d.outputDir)
+	err := d.sh("test", "-e", strconv.Quote(d.outputDir))
 	return err == nil, nil
 }
 
 func (d *ESX5Driver) ListFiles() ([]string, error) {
-	stdout, err := d.ssh("ls -1p "+d.outputDir, nil)
+	stdout, err := d.ssh("ls -1p "+ strconv.Quote(d.outputDir), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -397,11 +404,11 @@ func (d *ESX5Driver) MkdirAll() error {
 }
 
 func (d *ESX5Driver) Remove(path string) error {
-	return d.sh("rm", path)
+	return d.sh("rm", strconv.Quote(path))
 }
 
 func (d *ESX5Driver) RemoveAll() error {
-	return d.sh("rm", "-rf", d.outputDir)
+	return d.sh("rm", "-rf", strconv.Quote(d.outputDir))
 }
 
 func (d *ESX5Driver) SetOutputDir(path string) {
@@ -493,7 +500,7 @@ func (d *ESX5Driver) checkGuestIPHackEnabled() error {
 }
 
 func (d *ESX5Driver) mkdir(path string) error {
-	return d.sh("mkdir", "-p", path)
+	return d.sh("mkdir", "-p", strconv.Quote(path))
 }
 
 func (d *ESX5Driver) upload(dst, src string) error {
@@ -516,7 +523,7 @@ func (d *ESX5Driver) Download(src, dst string) error {
 
 func (d *ESX5Driver) verifyChecksum(ctype string, hash string, file string) bool {
 	if ctype == "none" {
-		if err := d.sh("stat", file); err != nil {
+		if err := d.sh("stat", strconv.Quote(file)); err != nil {
 			return false
 		}
 	} else {
